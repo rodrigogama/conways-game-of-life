2e045ff1140a0fcd43589db6041c7f2dd789791f
Rodrigo Gama <rodrigo.gama@xpi.com.br>
2019-12-16 16:30:27 -0300

    feat: better organization with constants

---
+++ src/helpers/constants/index.js
@@ -0,0 +1,12 @@
+export const NUM_ROWS = 30;
+export const NUM_COLUMNS = 30;
+export const GRID_OPERATIONS = [
+  [0, 1],   // right
+  [0, -1],  // left
+  [1, 0],   // top
+  [-1, 0],  // down
+  [1, -1],  // top right
+  [-1, 1],  // top left
+  [1, 1],   // bottom right
+  [-1, -1], // bottom left
+];
